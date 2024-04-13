a7ee36be85481467d2b814f6fab45b393def7510
James Nesbitt <james04nesbitt@gmail.com>
2024-04-13 16:25:53 -0400

    change

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .forms import ImageUploadForm
-from .content.Itinerary.myapp.forms import SurveyForm
+from .forms import SurveyForm
 from .forms import SurveyForm
 def index(request):
   template = loader.get_template('index.html')
